0874cdd7e474e72a8fcaed47369eadc570c9d4d3
shakthiganesh716-lgtm <shakthiganesh716@gmail.com>
2026-04-09 21:54:50 +0530

    UC8: Filter Passenger Bogies Using Streams

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,23 +1,22 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        Map<String, Integer> bogieCapacity = new HashMap<>();
-        bogieCapacity.put("Sleeper", 72);
-        bogieCapacity.put("AC Chair", 64);
-        bogieCapacity.put("First Class", 18);
-        bogieCapacity.put("Pantry Car", 0);
+        List<String> bogies = Arrays.asList(
+            "Sleeper", "AC Chair", "First Class",
+            "Rectangular Goods", "Cylindrical Goods", "Pantry Car"
+        );
 
-        System.out.println("Bogie Capacity Map:");
-        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
-            System.out.println("  " + entry.getKey() + " -> " + entry.getValue() + " seats");
-        }
+        List<String> passengerBogies = bogies.stream()
+            .filter(b -> b.equals("Sleeper") || b.equals("AC Chair") || b.equals("First Class"))
+            .collect(Collectors.toList());
 
-        System.out.println("\nCapacity of Sleeper: " + bogieCapacity.get("Sleeper"));
-        bogieCapacity.put("Sleeper", 80);
-        System.out.println("Updated Sleeper Capacity: " + bogieCapacity.get("Sleeper"));
+        System.out.println("All Bogies     : " + bogies);
+        System.out.println("Passenger Bogies: " + passengerBogies);
+        System.out.println("Total Passenger Bogies: " + passengerBogies.size());
     }
 }
